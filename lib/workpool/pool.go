8421ecfab641e49994591a68233488a5470323f9
kunwu <kunwu@pinduoduo.com>
2019-09-08 16:58:45 +0800

    feat(workpool): support async priority
    Signed-off-by: kunwu <kunwu@pinduoduo.com>

--- lib/workpool/pool.go
+++ lib/workpool/pool.go
@@ -37,7 +37,7 @@ type Pool interface {
 	Sync(context.Context, interface{}) (interface{}, error)
 
 	// Async add one job and callback function into the pool and return
-	Async(context.Context, interface{}, Callback) error
+	Async(context.Context, int, interface{}, Callback) error
 
 	// Scale changes the worker pool size
 	Scale(count int, force bool) error
@@ -54,6 +54,7 @@ var (
 	ErrPoolIsNotRunning = errors.New("pool is not running")
 	ErrPoolFull         = errors.New("workpool is full")
 	ErrScaleConflict    = errors.New("scale conflict")
+	ErrAsyncPriority    = errors.New("error async priority")
 )
 
 type Result struct {
@@ -65,6 +66,7 @@ type Result struct {
 
 type Task struct {
 	Async    bool
+	Priority int
 	Context  context.Context
 	Value    interface{}
 	Callback Callback
@@ -78,27 +80,31 @@ type Controller struct {
 
 type pool struct {
 	sync.RWMutex
-	wg          sync.WaitGroup
-	count       int64
-	async_block bool
-	sync_queue  chan Task
-	async_queue chan Task
-	callback    chan Result
-	handler     Handler
-	controllers []Controller
-	scaling     int64
-	running     int64
+	wg            sync.WaitGroup
+	count         int64
+	async_block   bool
+	sync_queue    chan *Task
+	async_queues  []chan *Task
+	async_waiting chan struct{}
+	callback      chan Result
+	handler       Handler
+	controllers   []Controller
+	scaling       int64
+	running       int64
 }
 
 // NewPool creates an instance of Pool
 // 	count       - worker number
+//  levels 		- async queue levels
 // 	aysnc_block - if async calls block
 //  handler 	- worker handle function
-func NewPool(count int, async_block bool, handler HandlerFunc) Pool {
+func NewPool(count, levels int, async_block bool, handler HandlerFunc) Pool {
 	return &pool{
-		count:       int64(count),
-		handler:     handler,
-		async_block: async_block,
+		count:         int64(count),
+		handler:       handler,
+		async_block:   async_block,
+		async_queues:  make([]chan *Task, levels),
+		async_waiting: make(chan struct{}),
 	}
 }
 
@@ -107,7 +113,7 @@ func (p *pool) Sync(ctx context.Context, job interface{}) (interface{}, error) {
 	if atomic.LoadInt64(&p.running) == 0 {
 		return nil, ErrPoolIsNotRunning
 	}
-	task := Task{
+	task := &Task{
 		Context: ctx,
 		Value:   job,
 		Result:  make(chan Result, 1),
@@ -125,28 +131,38 @@ func (p *pool) Sync(ctx context.Context, job interface{}) (interface{}, error) {
 	}
 }
 
-func (p *pool) Async(ctx context.Context, job interface{}, callback Callback) error {
+func (p *pool) Async(ctx context.Context, priority int, job interface{}, callback Callback) error {
 	if atomic.LoadInt64(&p.running) == 0 {
 		return ErrPoolIsNotRunning
 	}
-	task := Task{
+	if priority < 0 || priority >= len(p.async_queues) {
+		return ErrAsyncPriority
+	}
+	task := &Task{
 		Async:    true,
 		Context:  ctx,
 		Value:    job,
+		Priority: priority,
 		Callback: callback,
 	}
 	if p.async_block {
 		select {
 		case <-ctx.Done():
 			return ErrContextCanel
-		case p.async_queue <- task:
-			return nil
+		case p.async_waiting <- struct{}{}:
+		default:
 		}
+		p.async_queues[priority] <- task
+		return nil
 	} else {
 		select {
 		case <-ctx.Done():
 			return ErrContextCanel
-		case p.async_queue <- task:
+		case p.async_waiting <- struct{}{}:
+		default:
+		}
+		select {
+		case p.async_queues[priority] <- task:
 			return nil
 		default:
 			return ErrPoolFull
@@ -213,7 +229,9 @@ func (p *pool) Stop() {
 	p.controllers = []Controller{}
 	p.Unlock()
 	close(p.sync_queue)
-	close(p.async_queue)
+	for _, ch := range p.async_queues {
+		close(ch)
+	}
 	close(p.callback)
 	p.wg.Wait()
 }
@@ -230,8 +248,10 @@ func (p *pool) Start() {
 		p.Unlock()
 	}()
 
-	p.sync_queue = make(chan Task, 1)
-	p.async_queue = make(chan Task, p.count)
+	p.sync_queue = make(chan *Task, 1)
+	for i := range p.async_queues {
+		p.async_queues[i] = make(chan *Task)
+	}
 	p.callback = make(chan Result)
 
 	for i := 0; i < int(p.count); i++ {
@@ -253,28 +273,48 @@ func (p *pool) do(ctx context.Context, index int, done chan struct{}) {
 		close(done)
 		p.wg.Done()
 	}()
+Loop:
 	for {
 		var (
-			task   Task
+			task   *Task
 			result Result
 		)
-		select {
-		case <-ctx.Done():
-			return
-		case task = <-p.sync_queue:
-		default:
+	TaskWaiting:
+		for _, ch := range p.async_queues {
+			// check context first
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+			// check sync queue second
+			select {
+			case task = <-p.sync_queue:
+				break TaskWaiting
+			default:
+			}
+			select {
+			case task = <-ch:
+				break TaskWaiting
+			default:
+			}
+		}
+		if task == nil {
 			select {
 			case <-ctx.Done():
 				return
 			case task = <-p.sync_queue:
-			case task = <-p.async_queue:
+				break
+			case <-p.async_waiting:
+				continue Loop
 			}
 		}
+		//fmt.Printf("index: %d, task: %v\n", index, task)
 		select {
 		case <-ctx.Done():
 			if atomic.LoadInt64(&p.running) == 1 {
 				if task.Async {
-					p.async_queue <- task
+					p.async_queues[task.Priority] <- task
 				} else {
 					p.sync_queue <- task
 				}
--- lib/workpool/pool_test.go
+++ lib/workpool/pool_test.go
@@ -18,117 +18,134 @@ func TestPool(t *testing.T) {
 		atomic.AddInt64(&index, 1)
 		return value, nil
 	}
-	pool := NewPool(3, true, handler)
+	pool := NewPool(3, 1, true, handler)
 	pool.Start()
 
-	// sync call
-	var wg sync.WaitGroup
-	for i := 1; i < 6; i++ {
-		wg.Add(1)
-		go func(index int64) {
-			defer wg.Done()
-			value, err := pool.Sync(context.Background(), index)
-			assert.Nil(t, err)
-			assert.Equal(t, index, value.(int64))
-		}(int64(i))
-	}
-	wg.Wait()
-
-	// async call
-	index = 0
-	callback := func(ctx context.Context, result interface{}, err error) {
-		defer wg.Done()
-		assert.Nil(t, err)
-		assert.Equal(t, atomic.LoadInt64(&index), result.(int64))
-	}
-	for i := 1; i < 6; i++ {
-		wg.Add(1)
-		err := pool.Async(context.Background(), int64(i), callback)
-		assert.Nil(t, err)
-	}
-	wg.Wait()
-
-	// sync + async call: sync (1-5) + async(6-10)
-	index = 0
-	for i := 1; i < 11; i++ {
-		if i > 5 {
-			//async
-			if i == 6 {
-				// first async call sleep 10ms to ensure sync call reach
-				time.Sleep(time.Duration(10) * time.Millisecond)
-			}
-			wg.Add(1)
-			assert.Nil(t, pool.Async(context.Background(), int64(i), callback))
-		} else {
-			// sync
+	t.Run("sync call", func(t *testing.T) {
+		var wg sync.WaitGroup
+		for i := 1; i < 6; i++ {
 			wg.Add(1)
 			go func(index int64) {
 				defer wg.Done()
 				value, err := pool.Sync(context.Background(), index)
-				assert.Equal(t, index, value.(int64))
 				assert.Nil(t, err)
+				assert.Equal(t, index, value.(int64))
 			}(int64(i))
 		}
-	}
-	wg.Wait()
+		wg.Wait()
+	})
+
+	t.Run("async call", func(t *testing.T) {
+		var wg sync.WaitGroup
+		index = 0
+		callback := func(ctx context.Context, result interface{}, err error) {
+			defer wg.Done()
+			assert.Nil(t, err)
+			assert.Equal(t, atomic.LoadInt64(&index), result.(int64))
+		}
+		for i := 1; i < 6; i++ {
+			wg.Add(1)
+			err := pool.Async(context.Background(), 0, int64(i), callback)
+			assert.Nil(t, err)
+		}
+		wg.Wait()
+	})
+
+	t.Run("sync and async call", func(t *testing.T) {
+		var wg sync.WaitGroup
+		callback := func(ctx context.Context, result interface{}, err error) {
+			defer wg.Done()
+			assert.Nil(t, err)
+			assert.Equal(t, atomic.LoadInt64(&index), result.(int64))
+		}
+		// sync + async call: sync (1-5) + async(6-10)
+		index = 0
+		for i := 1; i < 11; i++ {
+			if i > 5 {
+				//async
+				if i == 6 {
+					// first async call sleep 10ms to ensure sync call reach
+					time.Sleep(time.Duration(10) * time.Millisecond)
+				}
+				wg.Add(1)
+				assert.Nil(t, pool.Async(context.Background(), 0, int64(i), callback))
+			} else {
+				// sync
+				wg.Add(1)
+				go func(index int64) {
+					defer wg.Done()
+					value, err := pool.Sync(context.Background(), index)
+					assert.Equal(t, index, value.(int64))
+					assert.Nil(t, err)
+				}(int64(i))
+			}
+		}
+		wg.Wait()
+	})
 }
 
 func TestPoolScale(t *testing.T) {
 	// worker=3, aysnc call 1, 2, 3(scale to 5),4,5,6(count=5?)
 	var (
-		pool Pool
-		wg   sync.WaitGroup
+		pool     Pool
+		wg       sync.WaitGroup
+		callback = func(ctx context.Context, result interface{}, err error) {
+			wg.Done()
+		}
 	)
-	var scaled int64
-	handler := func(ctx context.Context, job interface{}, async bool) (interface{}, error) {
-		value := job.(int)
-		if atomic.LoadInt64(&scaled) != 1 {
-			count := pool.Count()
-			assert.Equal(t, 3, count)
-		} else {
-			assert.Equal(t, 5, pool.Count())
+	t.Run("pool scale up", func(t *testing.T) {
+		var scaled int64
+		handler := func(ctx context.Context, job interface{}, async bool) (interface{}, error) {
+			value := job.(int)
+			if atomic.LoadInt64(&scaled) != 1 {
+				count := pool.Count()
+				assert.Equal(t, 3, count)
+			} else {
+				assert.Equal(t, 5, pool.Count())
+			}
+			time.Sleep(time.Duration(value*20) * time.Millisecond)
+			return value, nil
 		}
-		time.Sleep(time.Duration(value*20) * time.Millisecond)
-		return value, nil
-	}
-	callback := func(ctx context.Context, result interface{}, err error) {
-		wg.Done()
-	}
-	pool = NewPool(3, true, handler)
-	pool.Start()
-	for i := 1; i <= 6; i++ {
-		wg.Add(1)
-		assert.Nil(t, pool.Async(context.Background(), i, callback))
-	}
-
-	assert.NoError(t, pool.Scale(5, false))
-	atomic.StoreInt64(&scaled, 1)
-	wg.Wait()
-	pool.Stop()
-
-	// worker=3, aysnc call 1,2,3(scale to 2),4,5(count=2?)
-	atomic.StoreInt64(&scaled, 0)
-	handler = func(ctx context.Context, job interface{}, async bool) (interface{}, error) {
-		value := job.(int)
-		if atomic.LoadInt64(&scaled) != 1 {
-			count := pool.Count()
-			assert.Equal(t, 3, count)
-		} else {
-			assert.Equal(t, 2, pool.Count())
+		pool = NewPool(3, 1, true, handler)
+		pool.Start()
+		for i := 1; i <= 6; i++ {
+			wg.Add(1)
+			assert.Nil(t, pool.Async(context.Background(), 0, i, callback))
+			if i == 3 {
+				assert.NoError(t, pool.Scale(5, false))
+				atomic.StoreInt64(&scaled, 1)
+			}
 		}
+		wg.Wait()
+		pool.Stop()
+	})
+	t.Run("pool scale down", func(t *testing.T) {
+		var scaled int64
+		// worker=3, aysnc call 1,2,3(scale to 2),4,5(count=2?)
+		handler := func(ctx context.Context, job interface{}, async bool) (interface{}, error) {
+			value := job.(int)
+			if atomic.LoadInt64(&scaled) != 1 {
+				count := pool.Count()
+				assert.Equal(t, 3, count)
+			} else {
+				assert.Equal(t, 2, pool.Count())
+			}
 
-		time.Sleep(time.Duration(value*20) * time.Millisecond)
-		return value, nil
-	}
-	pool = NewPool(3, true, handler)
-	pool.Start()
-	for i := 1; i <= 6; i++ {
-		wg.Add(1)
-		assert.Nil(t, pool.Async(context.Background(), i, callback))
-	}
-	assert.NoError(t, pool.Scale(2, false))
-	atomic.StoreInt64(&scaled, 1)
-	wg.Wait()
+			time.Sleep(time.Duration(value*20) * time.Millisecond)
+			return value, nil
+		}
+		pool = NewPool(3, 1, true, handler)
+		pool.Start()
+		for i := 1; i <= 6; i++ {
+			wg.Add(1)
+			assert.Nil(t, pool.Async(context.Background(), 0, i, callback))
+			if i == 3 {
+				assert.NoError(t, pool.Scale(2, false))
+				atomic.StoreInt64(&scaled, 1)
+			}
+		}
+		wg.Wait()
+	})
 }
 
 func TestHandlerContextCancel(t *testing.T) {
@@ -141,65 +158,70 @@ func TestHandlerContextCancel(t *testing.T) {
 			return job, nil
 		}
 	}
-	pool := NewPool(1, true, handler)
+	pool := NewPool(1, 1, true, handler)
 	pool.Start()
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
+	t.Run("sync context cancel", func(t *testing.T) {
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			ctx, cancel := context.WithCancel(context.Background())
+			go func() {
+				time.Sleep(20 * time.Millisecond)
+				cancel()
+			}()
+			_, err := pool.Sync(ctx, 1)
+			assert.Equal(t, ErrContextCanel, err)
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			result, err := pool.Sync(context.Background(), 1)
+			assert.Nil(t, err)
+			assert.Equal(t, 1, result.(int))
+		}()
+		wg.Wait()
+	})
+
+	t.Run("async context cancel", func(t *testing.T) {
+		var wg sync.WaitGroup
+		callback := func(ctx context.Context, result interface{}, err error) {
+			defer wg.Done()
+			if result == nil {
+				assert.Equal(t, ErrContextCanel, err)
+				return
+			}
+			assert.Nil(t, err)
+		}
 		ctx, cancel := context.WithCancel(context.Background())
 		go func() {
-			time.Sleep(20 * time.Millisecond)
+			time.Sleep(40 * time.Millisecond)
 			cancel()
 		}()
-		_, err := pool.Sync(ctx, 1)
-		assert.Equal(t, ErrContextCanel, err)
-	}()
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		result, err := pool.Sync(context.Background(), 1)
-		assert.Nil(t, err)
-		assert.Equal(t, 1, result.(int))
-	}()
-	wg.Wait()
-
-	callback := func(ctx context.Context, result interface{}, err error) {
-		defer wg.Done()
-		if result == nil {
-			assert.Equal(t, ErrContextCanel, err)
-			return
-		}
-		assert.Nil(t, err)
-	}
-	ctx, cancel := context.WithCancel(context.Background())
-	go func() {
-		time.Sleep(40 * time.Millisecond)
-		cancel()
-	}()
-	wg.Add(1)
-	assert.Nil(t, pool.Async(ctx, 1, callback))
-	wg.Add(1)
-	assert.Nil(t, pool.Async(context.Background(), 2, callback))
-	wg.Wait()
+		assert.NoError(t, pool.Async(ctx, 0, 1, callback))
+		wg.Add(1)
+		assert.NoError(t, pool.Async(context.Background(), 0, 2, callback))
+		wg.Add(1)
+		wg.Wait()
+	})
 }
 
 func TestPoolScaleConflict(t *testing.T) {
 	var wg sync.WaitGroup
-	pool := NewPool(2, true, func(ctx context.Context, job interface{}, async bool) (interface{}, error) {
+	pool := NewPool(2, 1, true, func(ctx context.Context, job interface{}, async bool) (interface{}, error) {
 		time.Sleep(100 * time.Millisecond)
 		wg.Done()
 		return nil, nil
 	})
 	_, err := pool.Sync(context.Background(), 1)
 	assert.Equal(t, ErrPoolIsNotRunning, err)
-	assert.Equal(t, ErrPoolIsNotRunning, pool.Async(context.Background(), 1, nil))
+	assert.Equal(t, ErrPoolIsNotRunning, pool.Async(context.Background(), 0, 1, nil))
 	pool.Start()
 	wg.Add(1)
-	assert.Nil(t, pool.Async(context.Background(), 1, nil))
+	assert.Nil(t, pool.Async(context.Background(), 0, 1, nil))
 	wg.Add(1)
-	assert.Nil(t, pool.Async(context.Background(), 2, nil))
+	assert.Nil(t, pool.Async(context.Background(), 0, 2, nil))
 	go func() {
 		time.Sleep(50 * time.Millisecond)
 		err := pool.Scale(2, false)
@@ -209,7 +231,7 @@ func TestPoolScaleConflict(t *testing.T) {
 	assert.Nil(t, pool.Scale(1, false))
 	wg.Wait()
 	wg.Add(1)
-	assert.Nil(t, pool.Async(context.Background(), 1, nil))
+	assert.Nil(t, pool.Async(context.Background(), 0, 1, nil))
 	go func() {
 		time.Sleep(50 * time.Millisecond)
 		assert.Equal(t, ErrScaleConflict, pool.Scale(2, false))
@@ -221,15 +243,95 @@ func TestPoolScaleConflict(t *testing.T) {
 }
 
 func TestPoolAsyncFull(t *testing.T) {
-	pool := NewPool(1, false, func(ctx context.Context, job interface{}, async bool) (interface{}, error) {
+	pool := NewPool(1, 1, false, func(ctx context.Context, job interface{}, async bool) (interface{}, error) {
 		time.Sleep(50 * time.Millisecond)
 		return nil, nil
 	})
 	pool.Start()
-	assert.Nil(t, pool.Async(context.Background(), 1, nil))
-	time.Sleep(10 * time.Millisecond)
-	assert.Nil(t, pool.Async(context.Background(), 1, nil))
-	time.Sleep(10 * time.Millisecond)
-	assert.Equal(t, ErrPoolFull, pool.Async(context.Background(), 1, nil))
+	assert.Equal(t, ErrPoolFull, pool.Async(context.Background(), 0, 1, nil))
 	time.Sleep(100 * time.Millisecond)
+}
+
+func TestPoolAsyncPriority(t *testing.T) {
+	// sync:       10ms * 20
+	// priority 0: 10ms * 20
+	// priority 1: 10ms * 20
+	// priority 2: 10ms * 20
+	var (
+		s      int64 = 20
+		p1     int64 = 20
+		p2     int64 = 20
+		p3     int64 = 20
+		wg     sync.WaitGroup
+		finish = make(chan int, 4)
+	)
+	handler := func(ctx context.Context, job interface{}, async bool) (interface{}, error) {
+		value := job.(int)
+		time.Sleep(10 * time.Millisecond)
+		wg.Done()
+		return value, nil
+	}
+	pool := NewPool(3, 3, true, handler)
+	pool.Start()
+	for j := 0; j < 3; j++ {
+		go func() {
+			for {
+				if p := atomic.AddInt64(&s, -1); p > 0 {
+					wg.Add(1)
+					_, err := pool.Sync(context.Background(), 0)
+					assert.NoError(t, err)
+				} else {
+					if p == 0 {
+						finish <- 0
+					}
+					return
+				}
+			}
+		}()
+		go func() {
+			for {
+				if p := atomic.AddInt64(&p1, -1); p > 0 {
+					wg.Add(1)
+					assert.NoError(t, pool.Async(context.Background(), 0, 1, nil))
+				} else {
+					if p == 0 {
+						finish <- 1
+					}
+					return
+				}
+			}
+		}()
+		go func() {
+			for {
+				if p := atomic.AddInt64(&p2, -1); p > 0 {
+					wg.Add(1)
+					assert.NoError(t, pool.Async(context.Background(), 1, 2, nil))
+				} else {
+					if p == 0 {
+						finish <- 2
+					}
+					return
+				}
+			}
+		}()
+		go func() {
+			for {
+				if p := atomic.AddInt64(&p3, -1); p > 0 {
+					wg.Add(1)
+					assert.NoError(t, pool.Async(context.Background(), 2, 3, nil))
+				} else {
+					if p == 0 {
+						finish <- 3
+					}
+					return
+				}
+			}
+		}()
+	}
+
+	wg.Wait()
+	for i := 0; i < 4; i++ {
+		v := <-finish
+		assert.Equal(t, i, v)
+	}
 }
